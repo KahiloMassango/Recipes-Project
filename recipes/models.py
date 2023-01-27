4d8ce0d96dcd77cb2125d92a3608b04800167b37
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-01-27 20:37:45 +0100

    Django models and ORM

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django import forms
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+
+    Prep_unit_choices = (
+        ('MINUTE', 'Minute/s'),
+        ('HOUR', 'Hour/s'),)
+
+    Servings_unit_choices = (
+        ('PLATES','Plate/s'),
+        ('PEOPLE','People/s'),
+    )
+    # title description slug
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    # preparation_time preparation_time_unit
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=10, choices=Prep_unit_choices, default="MINUTE")
+    # servings servings_unit
+    servings = models.IntegerField()
+    serving_unit = models.CharField(max_length=10,  choices=Servings_unit_choices, default="PLATES")
+    # preparation_step
+    preparation_steps = models.TextField()
+    # preparation_step_is_html
+    preparation_step_is_html = models.BooleanField(default=False)
+    # created_at updated_at
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # is_published
+    is_published = models.BooleanField(default=False)
+    # cover
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
+    # category (Relação)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null =True, blank=True, default=None)
+    # Author (Relação)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipe.factory import make_recipe
+from .models import Recipe
 
 # Create your views here.
 
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+        ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': recipes
     })
 
 def recipe(request, id):
+    recipe = Recipe.objects.get(id=id)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_datail_page': True,
     })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category_id=category_id, 
+        is_published=True
+        ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes
+    }) 
