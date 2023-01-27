673db6623e58285114fbdfc47e6e25104bb98a3c
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-01-27 12:21:33 +0100

    herança, blocos, if, for, filtros e urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from utils.recipe.factory import make_recipe
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name':'kahilo'
+        'recipes': [make_recipe() for _ in range(10)]
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_datail_page': True,
     })
