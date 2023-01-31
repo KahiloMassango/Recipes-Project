ad892a938a7d827f1d4ccde0165c9b94b428a266
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-01-31 20:28:17 +0100

    Usando resolve para entender qual view function está sendo usada

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from . import views
 
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
@@ -15,3 +16,18 @@ class RecipeURLsTest(TestCase):
         self.assertEqual(url, '/recipes/99')
 
 
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_views_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_views_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_views_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 99}))
+        self.assertIs(view.func, views.recipe)
+
+        
