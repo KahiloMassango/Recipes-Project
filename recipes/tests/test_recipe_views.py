1291282525cff355b8f082e9e660594dc2ae2e8e
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-02-06 20:50:59 +0100

    some changes

---
+++ recipes/tests/test_recipe_views.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from recipes import views
+
+
+
+
+class RecipeViewsTest(TestCase):
+
+    # Testing views functions
+
+    def test_recipe_home_views_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_views_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_views_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 99}))
+        self.assertIs(view.func, views.recipe)
+
+    # <------------------end----------------------->
+
+    # Testing view status code
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+
+    # <------------------end----------------------->
+
+    # Testing view template render
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    
+
+    # <------------------end----------------------->
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            'No recipes found',
+            response.content.decode('utf-8'))
