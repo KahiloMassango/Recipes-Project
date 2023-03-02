9441c9fd347f711f21eed5ebe3b54462a8977d92
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-03-02 17:32:25 +0100

    solving a little mistake in urls

--- authors/views.py
+++ authors/views.py
@@ -3,12 +3,15 @@ from django.http import Http404
 from .forms import RegisterForm
 # Create your views here.
 
+
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None) 
     form = RegisterForm(register_form_data)
     return render(request, "authors/pages/register_view.html", {
-        'form':form
+        'form':form,
         })
+
+
 
 def register_create(request):
     if not request.POST:
@@ -17,5 +20,5 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
-
+    
     return redirect('authors:register')
