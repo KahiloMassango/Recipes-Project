491c229c329b000dd33fb4db237817da5d7d545e
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-03-14 19:02:50 +0100

    django form, labels, widgets, error_messages, help_texts

--- authors/forms.py
+++ authors/forms.py
@@ -11,5 +11,33 @@ class RegisterForm(forms.ModelForm):
             'username',
             'email',
             'password',
-
-        ]
+        ]   
+        labels = {
+            'username':'Username',
+            'first_name':'First name',
+            'last_name': 'Last name',
+            'email':'E-mail',
+            'password':'Password',
+        }
+        help_texts = {
+            'email':'O E-mail tem que ser válido'
+        }
+        error_messages = {
+            'username': {
+                'required':'Este campo não pode estar vazio.',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+            'placeholder':'First Name'
+            }),
+            'last_name': forms.TextInput(attrs={
+            'placeholder':'Last Name'
+            }),
+            'username': forms.TextInput(attrs={
+            'placeholder':'Username'
+            }),
+            'password': forms.PasswordInput(attrs={
+            'placeholder':'Type your password here'
+            }),
+        }
