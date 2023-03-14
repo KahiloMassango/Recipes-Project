b08b724a1d4dd6647d854780ff9a0921d761f6c4
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-03-14 19:46:49 +0100

    aplicando regras do flake8

--- authors/forms.py
+++ authors/forms.py
@@ -5,59 +5,48 @@ from django.contrib.auth.models import User
 class RegisterForm(forms.ModelForm):
     password = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(
-        attrs={
-        'placeholder':'Password'}),
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'}),
         help_text=(
             'Password must have at least one uppercase letter,'
             'one lowercase letter and one number.'
         ),
         error_messages={
-            'required':'Password must not be empty'
+            'required': 'Password must not be empty'
         }
         )
     password2 = forms.CharField(
         label='Confirm password',
         required=True,
-        widget=forms.PasswordInput(attrs={
-        'placeholder':'Confirm password'
-        })
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'})
         )
+
     class Meta:
         model = User
         fields = [
-            'first_name', 
+            'first_name',
             'last_name',
             'username',
             'email',
             'password',
-        ]   
+        ]
         labels = {
-            'username':'Username',
-            'first_name':'First name',
+            'username': 'Username',
+            'first_name': 'First name',
             'last_name': 'Last name',
-            'email':'E-mail',
-            'password':'Password',
+            'email': 'E-mail',
+            'password': 'Password',
         }
         help_texts = {
-            'email':'E-mail must be valid.'
+            'email': 'E-mail must be valid.'
         }
         error_messages = {
             'username': {
-                'required':'This fiels must not be empty.',
+                'required': 'This fiels must not be empty.',
             }
         }
         widgets = {
-            'first_name': forms.TextInput(attrs={
-            'placeholder':'First Name'
-            }),
-            'last_name': forms.TextInput(attrs={
-            'placeholder':'Last Name'
-            }),
-            'username': forms.TextInput(attrs={
-            'placeholder':'Username'
-            }),
-            'email':forms.EmailInput(attrs={
-            'placeholder':'E-mail'
-            })
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name'}),
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'E-mail'})
         }
